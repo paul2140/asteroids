4a6c12ed11aa3849950f2ea9d914118651f82725
git_PaulW <starsrift@netscape.net>
2024-11-07 01:34:10 -0800

    Full commit

--- main.py
+++ main.py
@@ -2,20 +2,46 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+import sys
 from constants import *
+from circleshape import *
+from player import *
+from asteroidfield import *
+from shot import Shot
 
 def main():
 	pygame.init()
+	clock = pygame.time.Clock()
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-	print("Starting asteroids!")
-	print(f"Screen width: {SCREEN_WIDTH}")
-	print(f"Screen height: {SCREEN_HEIGHT}")
+	
+	updatable = pygame.sprite.Group()
+	drawable = pygame.sprite.Group()
+	asteroids = pygame.sprite.Group()
+	shots = pygame.sprite.Group()
+	Player.containers = (updatable, drawable)
+	Asteroid.containers = (asteroids, updatable, drawable)
+	AsteroidField.containers = (updatable)
+	Shot.containers = (shots, updatable, drawable)
+
+	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+	dt = 0
+	asteroidfield = AsteroidField()
+
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
-		pygame.Surface.fill(screen, (0, 0, 0))
+		for obj in updatable:
+			obj.update(dt)
+		for obj in asteroids:
+			if obj.collided(player):
+				sys.exit("Game over!")
+		screen.fill("black")
+		for obj in drawable:
+			obj.draw(screen)
+		
 		pygame.display.flip()
+		dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
 		main()
